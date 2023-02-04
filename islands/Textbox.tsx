be7dcf28c0ac9bb5c2ad462dd1ad0b0f65d76155
Derik van Schaik <derikvanschaik@gmail.com>
2023-02-03 23:08:00 -0800

    add more ellipsis icon on hover

--- islands/Textbox.tsx
+++ islands/Textbox.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "preact/hooks";
+import More from '../components/More.tsx';
 
 interface TextboxProps{
     x: number,
@@ -8,12 +9,20 @@ interface TextboxProps{
     handleMouseup: () => void;
 }
 export default function Textbox({x, y, text, handleMousedown, handleMouseup}: TextboxProps) {
+  const [hover, setHover] = useState(false);
   return (
     <div
         onMousedown={handleMousedown}
         onMouseup={handleMouseup}
+        onMouseenter={() => setHover(true)}
+        onMouseleave={() => setHover(false)}
         className={`scale-110 fixed top-[${y}px] left-[${x}px] border-2 border-grey-500 shadow-md rounded-lg px-4 py-3 select-none transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 cursor-move bg-white`}>
     {text}
+
+    <div className={`absolute -top-1 -right-1 ${hover? 'visible': 'invisible'}`}>
+        <More />
+    </div>
+
     </div>
 
   );
