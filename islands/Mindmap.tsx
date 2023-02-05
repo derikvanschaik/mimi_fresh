7c05d577918ceb0588c860c0f64c38399aa599e9
Derik van Schaik <derikvanschaik@gmail.com>
2023-02-04 16:46:20 -0800

    islands/Path.tsx

--- islands/Mindmap.tsx
+++ islands/Mindmap.tsx
@@ -9,29 +9,16 @@ interface TextBox {
 }
 
 
-export default function Mindmap(props: any) {
+export default function Mindmap(props : any) {
   const [h, setH] = useState(0);
   const [w, setW] = useState(0);
   // rename this to tid or smthn
   const [tIdx, setTidx] = useState(null);
   const [pos, setPos] = useState({ x: 0, y: 0});
 
-  const TBOXES = [
-    { x: 600, y: 300, text: 'hello world', id: "90192", selected: false},
-    { x: 230, y: 89, text: 'Again hello!', id: "9102390", selected: false},
-    {x: 100, y: 100, text: 'hey there babyyy', id: "77", selected: false},
-    { x: 400, y: 400, text: 'new link', id: "78979", selected: false},
-    { x: 800, y: 700, text: 'yes baby we love new links!', id: "87", selected: false}
-  ]
-  const LINES = [
-    { from: {x: 600, y: 300}, to: {x: 230, y: 89}},
-    { from: {x: 100, y: 100}, to: {x: 230, y:89 }},
-    {from:{x: 400, y: 400}, to:{ x: 800, y: 700}},
-    {from:{x: 230, y: 89}, to:{ x: 800, y: 700}},
-  ]
 
-  const [textboxes, setTextboxes] = useState<TextBox[]>(TBOXES)
-  const [lines, setLines ] = useState(LINES);
+  const [textboxes, setTextboxes] = useState<TextBox[]>(props.textboxes)
+  const [lines, setLines ] = useState(props.lines);
 
   const canvasRef = useRef(null);
 
