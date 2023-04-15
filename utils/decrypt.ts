094b691f29b9716c4dce34231f95b8bfc6d2deea
Derik van Schaik <derikvanschaik@gmail.com>
2023-04-15 14:12:39 -0700

    change deno deploy query

--- utils/decrypt.ts
+++ utils/decrypt.ts
@@ -10,7 +10,7 @@ import {
     compareSync,
   } from "https://deno.land/x/bcrypt/mod.ts";
   
-export const isRunningInDenoDeploy = Deno.permissions?.query === undefined; // This is crude check for if the code in running in Deno Deploy. It works for now but may not work in the future.
+  export const isRunningInDenoDeploy = (globalThis as any).Worker === undefined; 
 console.log('is running in deploy = ', isRunningInDenoDeploy)
 
 export const hash: typeof hashPromise = isRunningInDenoDeploy
