e1a08a4e413d8a477d68cd4641797014f3cd570a
Derik van Schaik <derikvanschaik@gmail.com>
2023-06-24 12:26:18 -0700

    move submit button out of component

--- routes/register.tsx
+++ routes/register.tsx
@@ -29,6 +29,8 @@ export default function Register({ data }) {
                           </div>
                           <PasswordChooser />
                           
+                          <button type="submit" class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-white">Register</button>
+                          
                       </form>
                   </div>
               </div>
